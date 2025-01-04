d08c3bd754ab105616952943cce9c0c9483ac154
Ganesh Ghode <ganeshghode2002@gmail.com>
2025-01-04 12:20:02 +0530

    "2D array Questions"

--- 2DArray/minVlaue.cpp
+++ 2DArray/minVlaue.cpp
@@ -14,18 +14,45 @@ using namespace std;
 //     return minVal;
 // }
 
-int findMaxFrom2D(int arr[][4], int row, int col)
+// int findMaxFrom2D(int arr[][4], int row, int col)
+// {
+//     int maxVal = INT8_MIN;
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < col; j++)
+//         {
+//             maxVal = max(maxVal, arr[i][j]);
+//         }
+//     }
+//     return maxVal;
+// }
+// void rowWiseSum(int arr[][4], int row, int col)
+// {
+//     for (int i = 0; i < row; i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < col; j++)
+//         {
+//             sum = sum + arr[i][j];
+//         }
+//         cout<<sum;
+//         cout<<endl;
+//     }
+// }
+void colWiseSum(int arr[][4], int row, int col)
 {
-    int maxVal = INT8_MIN;
     for (int i = 0; i < row; i++)
     {
+        int sum = 0;
         for (int j = 0; j < col; j++)
         {
-            maxVal = max(maxVal, arr[i][j]);
+            sum = sum + arr[j][i];
         }
+        cout<<sum;
+        cout<<endl;
     }
-    return maxVal;
 }
+
 int main()
 {
     int arr[4][4] = {{1, 2, 3, 4},
@@ -34,6 +61,7 @@ int main()
                      {7, 23, 22, 30}};
     int row = 4, col = 4;
     // int minVal = findMinFrom2D(arr, row, col);
-    int maxVal = findMaxFrom2D(arr, row, col);
-    cout << "the minimum: " << maxVal;
+    // int maxVal = findMaxFrom2D(arr, row, col);
+    // rowWiseSum(arr, row, col);
+    colWiseSum(arr,row,col);
 }
