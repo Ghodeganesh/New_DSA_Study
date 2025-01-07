38a46fffcaa3813d1cd3ac294e28bc0313519e04
Ganesh Ghode <ganeshghode2002@gmail.com>
2025-01-07 11:34:16 +0530

    "first occurence"

---
+++ BinarySearch/firstOccurence.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout<<"hello";
+    int arr[] = {1, 2, 2,33, 33, 44, 54, 66};
+    int size = sizeof(arr) / sizeof(int);
+    cout << "enter target: ";
+    int target;
+    cin >> target;
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+    int ans = -1;
+
+    while (start <= end)
+    {
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else if (arr[mid] > target)
+        {
+            end = mid - 1;
+        }
+         mid = (start + end) / 2;
+    }
+
+    if(ans==-1){
+        cout<<"the element is not present";
+    }else{
+        cout<<"first occurence: "<<ans;
+    }
+}
